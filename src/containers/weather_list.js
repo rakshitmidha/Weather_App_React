f15449e03b66a11a54afac2374dc1a4c1ff60fa8
Rakshit <rakshit@infrrd.ai>
2017-06-14 16:25:12 +0530

    adding units & converting Fahrenheit to celcius

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,7 +5,7 @@ import Chart from '../components/chart';
  class WeatherList extends Component {
    renderWeather(cityData) {
      const name = cityData.city.name;
-     const temps = cityData.list.map(weather => weather.main.temp);
+     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp-273);
      const pressures = cityData.list.map(weather => weather.main.pressure);
      const humidities = cityData.list.map(weather => weather.main.humidity);
 
@@ -13,13 +13,13 @@ import Chart from '../components/chart';
      <tr key = {name}>
      <td>{name}</td>
      <td>
-        <Chart data = {temps} color = "orange" />
+        <Chart data = {temps} color = "orange" units = "C" />
      </td>
      <td>
-        <Chart data = {pressures} color = "green" />
+        <Chart data = {pressures} color = "green" units = "hPa" />
      </td>
      <td>
-        <Chart data = {humidities} color = "blue" />
+        <Chart data = {humidities} color = "blue" units = "%" />
      </td>
      </tr>
      );
@@ -31,9 +31,9 @@ import Chart from '../components/chart';
       <thead>
       <tr>
       <th>City</th>
-      <th>Temperature</th>
-      <th>Pressure</th>
-      <th>Humidity</th>
+      <th>Temperature (C)</th>
+      <th>Pressure (hPa)</th>
+      <th>Humidity (%)</th>
       </tr>
       </thead>
       <tbody>
